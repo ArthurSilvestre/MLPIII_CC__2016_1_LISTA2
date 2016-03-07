9a1225c38c7471bac7c25889557fc73f339a6d2d
Arthur <arthursilvestre@outlook.com>
2016-03-07 09:13:48 -0300

    1.2

--- 1/src/Data.java
+++ 1/src/Data.java
@@ -1,9 +1,24 @@
-
+//arquivo: Data.java
 public class Data {
+	public int dia;
+	public int mes;
+	public int ano;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Data() {}
+	
+	public Data(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
 	}
-
+	
+	public Data(int dia, int mes) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = 2016;
+	}
+	
+	public String toString() {
+		return dia + "/" + mes + "/" + ano;
+	}
 }
---
+++ 1/src/Principal.java
@@ -0,0 +1,10 @@
+//arquivo: Principal.java
+public class Principal {
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.dia = 8;
+		d.mes = 88;
+		d.ano = -1;
+		System.out.println(d);
+	}
+}
